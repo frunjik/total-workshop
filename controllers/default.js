20a5027b7253f8f0cdb8744d5f293810d5da56ea
frunjik <frunjik@gmail.com>
2021-08-21 09:35:13 +0200

    navigation wip

--- controllers/default.js
+++ controllers/default.js
@@ -1,3 +1,7 @@
 exports.install = function() {
 	ROUTE('+GET /');
+	ROUTE('+GET /explorer', 'index');
+	ROUTE('+GET /designer', 'index');
+	ROUTE('+GET /browser', 'index');
+	ROUTE('+GET /main', 'index');
 };
